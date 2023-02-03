68579e7288790810aa32ea7e25faee8e28d94107
Ricardo Rubio <ricardo.rubio@circulodecredito.com.mx>
2023-02-03 11:39:07 -0600

    Call to report segment removed

--- src/test/java/com/cdc/apihub/mx/RCC_FS/simulacion/test/ApiTest.java
+++ src/test/java/com/cdc/apihub/mx/RCC_FS/simulacion/test/ApiTest.java
@@ -1,7 +1,9 @@
 package com.cdc.apihub.mx.RCC_FS.simulacion.test;
 
-import okhttp3.OkHttpClient;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,20 +12,11 @@ import com.cdc.apihub.mx.RCC_FS.simulacion.client.ApiClient;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.ApiException;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.api.RCCFSApi;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.CatalogoEstados;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Consultas;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Creditos;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.DomicilioPeticion;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.DomiciliosRespuesta;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Empleos;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Mensajes;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.PersonaPeticion;
 import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Respuesta;
-import com.cdc.apihub.mx.RCC_FS.simulacion.client.model.Scores;
 
-import org.junit.Assert;
-import org.junit.Before;
-
-import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
 
 public class ApiTest {
 
@@ -86,34 +79,6 @@ public class ApiTest {
 			logger.info(response.toString());
 			Assert.assertTrue(response.getFolioConsulta() != null);
 			
-
-			if (response.getFolioConsulta() != null && !xFullReport) {
-				String folioConsulta = response.getFolioConsulta();
-
-				Consultas consultas = api.getConsultas(folioConsulta, xApiKey);
-				Assert.assertTrue(consultas.getConsultas() != null);
-				logger.info(consultas.toString());
-
-				Creditos creditos = api.getCreditos(folioConsulta, xApiKey);
-				Assert.assertTrue(creditos.getCreditos() != null);
-				logger.info(creditos.toString());
-
-				DomiciliosRespuesta domicilios = api.getDomicilios(folioConsulta, xApiKey);
-				Assert.assertTrue(domicilios.getDomicilios() != null);
-				logger.info(domicilios.toString());
-
-				Empleos empleos = api.getEmpleos(folioConsulta, xApiKey);
-				Assert.assertTrue(empleos.getEmpleos() != null);
-				logger.info(empleos.toString());
-
-				Scores scores = api.getScores(folioConsulta, xApiKey);
-				Assert.assertTrue(scores.getScores() != null);
-				logger.info(scores.toString());
-
-				Mensajes mensajes = api.getMensajes(folioConsulta, xApiKey);
-				Assert.assertTrue(mensajes.getMensajes() != null);
-				logger.info(mensajes.toString());
-			}
 		} catch (ApiException e) {
 			logger.error(e.getResponseBody());
 		}
